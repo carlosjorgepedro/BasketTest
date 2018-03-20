f4cdb1dd8bd02045909749442e28f5bf181bd559
Carlos Pedro <carlospedr@gmail.com>
2018-03-20 22:11:18 +0000

    Add Specflow project and feature file from test specs

---
+++ IntegrationTests/Basket.feature
@@ -0,0 +1,26 @@
+﻿Feature: Basket integration test
+
+Scenario: Butter and milk no discount
+	Given the basket has 1 bread
+	And 1 butter
+	And 1 milk
+	When I total the basket
+	Then the total should be £2.95
+
+Scenario: Butter and bread discount
+	Given the basket has 2 butter
+	And 2 bread
+	When I total the basket
+	Then the total should be £3.10
+
+Scenario: Milk discount
+	Given the basket has 4 milk
+	When I total the basket
+	Then the total should be £3.45
+
+Scenario: Two milk discount
+	Given the basket has 2 butter
+	And 1 bread
+	And 8 milk
+	When I total the basket
+	Then the total should be £9.00  
---
+++ IntegrationTests/BasketStepDefinition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Basket.IntegrationTests
+{
+    [Binding]
+    public sealed class BasketStepDefinition
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        [Given("the basket has (.*) bread")]
+        [Given("(.*) bread")]
+        public void AddBread(int count)
+        {
+        }
+
+        [Given("the basket has (.*) milk")]
+        [Given("(.*) milk")]
+        public void AddMilk(int count)
+        {
+        }
+
+        [Given("the basket has (.*) butter")]
+        [Given("(.*) butter")]
+        public void AddButter(int count)
+        {
+        }
+
+        [When("I total the basket")]
+        public void GetTotal()
+        {
+        }
+
+        [Then("the total should be £(.*)")]
+        public void ThenTheResultShouldBe(decimal expectedTotal)
+        {
+        }
+    }
+}
