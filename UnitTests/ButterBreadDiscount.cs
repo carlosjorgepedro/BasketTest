8b134d2a092275308088ac1c480bbda862168222
Carlos Pedro <carlospedr@gmail.com>
2018-03-21 00:25:38 +0000

    Implement Bread Butter Offer

---
+++ UnitTests/ButterBreadDiscount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.UnitTests
+{
+    public class ButterBreadDiscount : IDiscount
+    {
+        public decimal GetDiscount(List<BasketItem> basketItem)
+        {
+            var butterCount = basketItem
+                .Where(x => x.Product.Name == "butter")
+                .Sum(x => x.Count);
+
+            var breadCount = basketItem
+                .Where(x => x.Product.Name == "bread")
+                .Sum(x => x.Count);
+
+            var maxDiscountBreads = butterCount / 2;
+            var discountedBreads = breadCount > maxDiscountBreads ? maxDiscountBreads : breadCount;
+
+            return discountedBreads * basketItem.First(x => x.Product.Name == "bread").Product.Price / 2;
+        }
+    }
+}
---
+++ UnitTests/ButterBreadDiscountTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+
+namespace Basket.UnitTests
+{
+    [TestFixture]
+    public class ButterBreadDiscountTests
+    {
+        private readonly Product _butter = new Product("butter", 10m);
+        private readonly Product _bread = new Product("bread", 12m);
+
+        [TestCase(2, 1, 6)]
+        [TestCase(4, 1, 6)]
+        [TestCase(4, 2, 12)]
+        [TestCase(0, 2, 0)]
+        [TestCase(10, 0, 0)]
+        public void CalculateDiscount(int butterCount, int breadCount, decimal expectedDiscount)
+        {
+            var basketItems = new List<BasketItem> {
+                new BasketItem(_butter, butterCount),
+                new BasketItem(_bread, breadCount)
+                };
+            var discount = new ButterBreadDiscount();
+            var discountValue = discount.GetDiscount(basketItems);
+
+            Assert.AreEqual(expectedDiscount, discountValue);
+        }
+    }
+}
